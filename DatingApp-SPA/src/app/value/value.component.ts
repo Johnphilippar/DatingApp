24d0359975820b27e854995d5dd2063796deefe8
Alyssa Par <johnphilippar@gmail.com>
2020-05-04 14:22:24 +0800

    Updated Dating App

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -8,21 +8,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ValueComponent implements OnInit {
 
-
   values : any;
-
-  constructor(private http : HttpClient ) { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit() {
     this.getValues();
   }
 
   getValues(){
-    this.http.get('http://localhost:51020/api/values').subscribe(response => {
-      this.values = response;
-    },error => {
-      console.log(error)
-    })
+    this.http.get('http://localhost:51020/api/values').subscribe(Response =>{
+      this.values = Response;
+    }), e => {
+      console.log(e)
+    }
   }
+
+
 
 }
